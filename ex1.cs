e07526b5deee1c6e1703dd4b772cda8c1b706347
clas1k3-21 <clasik060708@gmail.com>
2025-04-29 19:46:55 +0300

    Add files via upload

---
+++ ex1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount("123456", 1000);
+            account.Deposit(500);
+            account.Withdraw(111300);
+            Console.WriteLine(account.Balance); 
+
+        }
+
+        class BankAccount 
+        { 
+        private string AccountNumber {  get; set; }
+        public double Balance {  get; private set; }
+
+            public BankAccount(string AccountNumber, double Balance)
+            {
+                this.AccountNumber = AccountNumber;
+                this.Balance = Balance; 
+                
+            }
+
+            public void Deposit(int amount) 
+            {
+                Balance += amount; 
+            }
+
+            public void Withdraw(int amount) 
+            { 
+                Balance -= amount;
+
+                if (Balance < 0) 
+                {
+                    Balance += amount;
+                    Console.WriteLine("Операция невозможна, на карте недостаточно средств");
+                }
+            }
+
+        }
+    }
+}
---
+++ ex2.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person("Alice");
+            //p.Age = -5;   
+            //p.Age = 15;
+            p.Age = 18;
+            Console.WriteLine(p.Age); 
+
+
+        }
+
+
+    }
+
+    class Person 
+    {
+    private int age;    
+    public string Name { get; set; }    
+    public int Age
+        {
+            get { return age; }
+
+            set
+            {
+
+                if (value < 0)
+                {
+                    Console.WriteLine("Возраст не может быть отрицательным");
+                    return;
+                }
+
+                if (value < 18)
+                {
+                    Console.WriteLine("Несовершеннолетний пользователь");
+                }
+                age = value;                  
+            }
+        }
+        public Person(string Name)
+        {
+            this.Name = Name;
+            
+        }
+
+    }
+}
